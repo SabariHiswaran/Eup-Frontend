e726a28707e774f42ed0528931a53608ce7ca337
Sabari <sabarihiswaran555@gmail.com>
2023-08-15 17:37:22 +0530

    menu in process

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
+import { Container } from 'react-bootstrap';
+import {IoIosArrowDropdownCircle} from 'react-icons/io'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className=" d-flex justify-content-center">
+
+        <p> Employee Uptrain Platform</p>
+
+      </Container>
+
+      <Container className=" d-flex justify-content-center">
+
+        <IoIosArrowDropdownCircle/>
+
+      </Container>
+
+    </>
+
   );
 }
 
